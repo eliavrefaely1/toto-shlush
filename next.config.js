0d01b3a150dd747828da1ae752c537448403377c
eliav <eliavr@hit.ac.il>
2025-09-11 01:11:10 +0300

    🎨 תיקון עיצוב Tailwind CSS - הפרויקט עובד עם עיצוב מלא

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [],
-  },
-  // תמיכה בעברית
-  i18n: {
-    locales: ['he'],
-    defaultLocale: 'he',
   },
 }
 
